7db5cc82d3f4dc5f91a3ac1c36ff4ed1f0f2108b
alexmeladze <nesiakobadze@gmail.com>
2026-03-26 22:54:18 +0400

    second commit

---
+++ level 11/classwork/cw.py
@@ -0,0 +1,6 @@
+listt = [12, 5, 3, 6, 9, 1, 10, 14, 15, 20]
+print(listt[2:6])
+
+print(listt[1:4])
+
+print(listt[-1:0])
---
+++ level 11/homework/hm.py
@@ -0,0 +1,85 @@
+# my_list = []
+
+# for i in range(5):
+#     number = int(input("Enter a number: "))
+#     my_list.append(number)
+
+# my_list.sort()
+# smallest = my_list.pop(0)
+
+# print("Removed number:", smallest)
+# print("Remaining list:", my_list)
+
+
+
+# my_list = []
+
+# for i in range(6):
+#     number = int(input("Enter a number: "))
+#     my_list.append(number)
+
+# count_10 = my_list.count(10)
+# print("10 is in the list", count_10, "times")
+
+# if count_10 > 2:
+#     my_list.remove(10)
+#     print("One 10 is removed")
+
+# print("The list now:", my_list)
+
+
+
+
+# my_list = [1, 2, 3, 4]
+# copy_list = my_list.copy()
+# copy_list.append(100)
+# print("Original list:", my_list)
+# print("Copied list:", copy_list)
+
+
+
+# my_list = []
+
+# for i in range(5):
+#     number = int(input("Enter a number: "))
+#     my_list.append(number)
+
+# my_list.sort()
+
+# if my_list[0] < 0:
+#     my_list.clear()
+
+# print("List now:", my_list)
+
+
+
+
+# my_list = []
+
+# for i in range(7):
+#     my_list.append(int(input("Enter a number: ")))
+
+# my_list.sort()
+# print(my_list.count(my_list[-1]))
+# my_list.remove(my_list[-1])
+# print(my_list)
+
+
+
+# my_list = [1, 2, 3, 4, 5, 6,]
+
+# print(my_list[0:3])
+
+
+
+
+
+# my_list = [1, 2, 3, 4, 5, 6, 7, 8]
+
+# print(my_list[4:8])
+
+
+
+my_list = [1, 2, 3, 4, 5, 6, 7]
+
+print(my_list[2:5])
