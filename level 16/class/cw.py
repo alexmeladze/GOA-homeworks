8bf4ec658450da2ee6e7b077df096e1537ae6cd8
alexmeladze <nesiakobadze@gmail.com>
2026-03-26 22:56:16 +0400

    second commit

---
+++ level 16/class/cw.py
@@ -0,0 +1,49 @@
+# def make_negative( number ):
+
+#     if number == 0:
+#         return 0
+#     elif number <= -1:
+#         return number
+#     elif number >= 1:
+#         return number * -1
+    
+# def switch_it_up(number):
+#     if number == 0:
+#         return "Zero"
+#     elif number == 1:
+#         return "One"
+#     elif number ==2:
+#         return "Two"
+#     elif number == 3:
+#         return "Three"
+#     elif number == 4:
+#         return "Four"
+#     elif number == 5:
+#         return "Five"
+#     elif number == 6:
+#         return "Six"
+#     elif number == 7:
+#         return "Seven"
+#     elif number == 8:
+#         return "Eight"
+#     elif number == 9:
+#         return "Nine"
+    
+
+
+
+
+    # def between(a,b):
+    #     result = []
+    # for SixSeven in range(a,b+1,1):
+    #     result.append(SixSeven)
+    # return result
+
+
+   
+
+
+# ხვალინდელი დავალება
+
+
+
---
+++ level 16/home/hw.py
@@ -0,0 +1,94 @@
+# def make_negative( number ):
+
+#     if number == 0:
+#         return 0
+#     elif number <= -1:
+#         return number
+#     elif number >= 1:
+#         return number * -1
+    
+# def switch_it_up(number):
+#     if number == 0:
+#         return "Zero"
+#     elif number == 1:
+#         return "One"
+#     elif number ==2:
+#         return "Two"
+#     elif number == 3:
+#         return "Three"
+#     elif number == 4:
+#         return "Four"
+#     elif number == 5:
+#         return "Five"
+#     elif number == 6:
+#         return "Six"
+#     elif number == 7:
+#         return "Seven"
+#     elif number == 8:
+#         return "Eight"
+#     elif number == 9:
+#         return "Nine"
+    
+
+
+
+
+    # def between(a,b):
+    #     result = []
+    # for SixSeven in range(a,b+1,1):
+    #     result.append(SixSeven)
+    # return result
+
+
+    # def create_array(n):
+    #     res=[]
+    # i=0
+    # while i<n:
+    #     i+=1
+    #     res.append(i)
+    # return res
+
+
+
+
+
+# def likes(names):
+
+#     if names == []:
+
+#         return "no one likes this"
+
+#     elif len(names) == 1:
+
+#         return f"{names[0]} likes this"
+
+#     elif len(names) == 2:
+
+#         return f"{names[0]} and {names[1]} like this"
+
+#     elif len(names) == 3:
+
+#         return f"{names[0]}, {names[1]} and {names[2]} like this"
+
+#     else:
+
+#         return f"{names[0]}, {names[1]} and {len(names) - 2 } others like this"
+
+
+
+
+# def count_by(x, n):
+#     new_list = []
+#     sum = x * n
+#     for i in range(x,sum+1,x):
+#         new_list.append(i)
+#     return new_list
+
+
+
+
+# def between(a,b):
+#     new_list = []
+#     for i in range(a,b+1,1):
+#         new_list.append(i)
+#     return new_list
