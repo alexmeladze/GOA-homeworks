66efcf020f364399c65113b1d70d9b016595b650
alexmeladze <nesiakobadze@gmail.com>
2026-03-26 22:53:53 +0400

    second commit

---
+++ level 10/hw.py
@@ -0,0 +1,189 @@
+
+# my_list = []
+
+
+# for i in range(3):
+#     number = int(input("Enter the number and press Enter: "))
+#     my_list.append(number)
+
+
+# print("Your list:", my_list)
+
+
+
+# list1 = ["10, 2, 5, 6, 1"]
+
+# print(list1)
+
+# list1.clear()
+
+# print(list1)
+
+
+
+
+# original_list = [1, 2, 3, 4]
+
+
+# copied_list = original_list.copy()
+
+
+# copied_list[0] = 99
+
+
+# print("Original list:", original_list)
+# print("Copied list:", copied_list)
+
+
+
+
+# my_list = [2, 5, 3, 2, 8, 2, 5]
+
+
+# number_to_count = int(input("Enter the number you want to count.: "))
+
+
+# count = my_list.count(number_to_count)
+
+
+# print("Number", number_to_count, "shows up", count, "times.")
+
+
+
+
+
+# my_list = [10, 20, 30, 40]
+
+
+# removed_item = my_list.pop()
+
+
+# print("Removed element:", removed_item)
+
+
+# print("New list:", my_list)
+
+
+
+
+
+# my_list = [5, 10, 15, 10, 20]
+
+
+# number_to_remove = int(input("Input the number you want to delete: "))
+
+
+# my_list.remove(number_to_remove)
+
+
+# print("New list:", my_list)
+
+
+
+
+# l = [9, 1, 4, 6, 2]
+
+# print(l)
+
+# l.reverse()
+
+# print(l)
+
+
+
+
+# my_list = [42, 7, 19, 3, 28]
+
+
+# my_list.sort()
+# print("List in ascending order:", my_list)
+
+
+# my_list.sort(reverse=True)
+# print("List in descending order:", my_list)
+
+
+
+
+
+# my_list = []
+
+
+# for i in range(5):
+#     number = int(input("Input a number and press Enter: "))
+#     my_list.append(number)
+
+
+# my_list.sort()
+
+
+# my_list.pop()
+
+
+# print("The final list:", my_list)
+
+
+
+
+# my_list = []
+
+
+# for i in range(6):
+#     number = int(input("Input a number: "))
+#     my_list.append(number)
+
+
+# count_5 = my_list.count(5)
+# print("The number 5 shows up", count_5, "times.")
+
+
+# if 5 in my_list:
+#     my_list.remove(5)
+
+
+# print("New list:", my_list)
+
+
+
+
+# original_list = [10, 20, 30, 40, 50]
+
+
+# copied_list = original_list.copy()
+
+
+
+# print("Original list:", original_list)
+# print("Copied list:", copied_list)
+
+
+
+
+# my_list = []
+
+
+# for i in range(4):
+#     number = int(input("Input a number: "))
+#     my_list.append(number)
+
+
+# if sum(my_list) > 100:
+#     my_list.clear()
+
+
+# print("List:", my_list)
+
+
+
+# my_list = []
+
+# for i in range(5):
+#     number = int(input("Enter a number: "))
+#     my_list.append(number)
+
+# my_list.sort()
+# my_list.remove(my_list[-1])
+
+# print("Final list:", my_list)
+
+
