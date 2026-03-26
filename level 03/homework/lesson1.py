543f27c4ea2194995a4f20563e9b6f1b06a811e1
alexmeladze <nesiakobadze@gmail.com>
2026-03-26 22:48:16 +0400

    second commit

---
+++ level 03/homework/lesson1.py
@@ -0,0 +1,48 @@
+#ამოცანა: მომხმარებელმა უნდა შეიყვანოს თავისი სახელი და პროგრამამ მას მიესალმოს.
+# name = input("Hello, What's your name?")
+# print("Hello " + name+ " it's nice to meet you"+ "!")
+
+
+#ამოცანა: მომხხმარებელმა შეიყვანოს რიცხვი და პროგრამამ ეგ რიცხვის 2-ჯერ გამრავლებული პასუკი გამოიტანოს.
+# # Ask the user to enter a number
+# number = float(input("Enter a number: "))
+
+# # Multiply the number by 2
+# result = number * 2
+
+# # Print the result
+# print("Twice the number is:", result)
+
+#Input - ის გამოყენებით, გააკეთეთ 5 მაგალითი თითო მათემათიკურ ოპერატორზე.
+# მიმატება.
+# a = float(input("Insert the first number: "))
+# b = float(input("Insert the second number: "))
+# print(a + b)
+
+
+# გამოკლება.
+# a = float(input("პირველი რიცხვი: "))
+# b = float(input("მეორე რიცხვი: "))
+# print(a - b)
+
+
+
+# გამრავლება
+# a = float(input("First number: "))
+# b = float(input("Second number: "))
+# print(a * b)
+
+
+
+
+# გაყოფა (Float)
+# a = float(input("First number: "))
+# b = float(input("Second number: "))
+# print(a / b)
+
+
+
+# გაყოფა (Int)
+a = int(input("Number: "))
+b = int(input("Number: "))
+print(a // b)
