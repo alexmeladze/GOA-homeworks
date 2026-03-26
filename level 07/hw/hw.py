69bfe1c008eb570b0d15e56495f8f3bae7401f73
alexmeladze <nesiakobadze@gmail.com>
2026-03-26 22:49:38 +0400

    second commit

---
+++ level 07/hw/hw.py
@@ -0,0 +1,106 @@
+# total = 0
+
+# for i in range(1, 6):
+#     print(i)
+#     total = total + i
+
+# print("Sum:", total)
+
+
+
+
+# password = ""
+
+# while password != "1234":
+#     password = input("Enter password: ")
+
+# print("Access granted")
+
+
+
+
+
+
+
+# n = int(input("Enter a number: "))
+
+# for i in range(1, n + 1):
+#     print(i)
+
+
+
+
+
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# c = int(input("Enter third number: "))
+
+# average = (a + b + c) / 3
+# print(average)
+
+
+
+
+# apple = input("Input anything: ")
+# banana = input("Input anything: ")
+# Green_Grape = input("Input anything: ")
+
+
+
+# print(type(apple))
+# print(type(banana))
+# print(type(Green_Grape))
+
+
+
+
+
+
+# num = int(input("Input a integer: "))
+
+# num1 = int(input("Input another integer: "))
+
+# if num > 5:
+#     print("The first number is more than 5")
+# else: print("The first number is not more than 5")
+
+
+# if num1 < 10:
+#     print("The second number is less than 10")
+# else: print("The second number is more than 10")
+
+
+
+# result = 1
+
+# for i in range(1, 6):
+#     result = result * i
+
+# print(result)
+
+
+
+
+
+
+
+
+
+# i = 0
+
+# while i < 10:
+#     print("Hello")
+#     i = i + 1
+
+
+
+
+
+total = 0
+
+while total < 50:
+    number = int(input("Enter a number: "))
+    total = total + number
+
+print("Sum:", total)
