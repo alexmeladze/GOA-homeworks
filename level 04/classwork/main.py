3b1e7c9a406f95d05decfef2b944bfb7e2383368
alexmeladze <nesiakobadze@gmail.com>
2026-03-26 22:47:35 +0400

    second commit

---
+++ level 04/classwork/main.py
@@ -0,0 +1,21 @@
+# print(5 > 3)
+
+# print(46 < 30)
+
+# print(90 >120)
+
+
+
+
+# print(2 ** 2)
+
+# print(9 ** 3)
+
+# print(3 ** 4)
+
+
+print(20 % 6)
+
+print(10 % 7)
+
+print(3 % 2)
---
+++ level 04/homework/main.py
@@ -0,0 +1,148 @@
+# N1
+
+
+
+# age = int(input("What's your age: "))
+
+# if age >= 18:
+#     print("You are an adult, Congrats")
+# else: print("You are under 18 years old")
+
+# print(type(age))
+
+
+
+
+# N2 ?
+
+
+# N3
+
+
+# nums = int(input("Input a number of your choice: "))
+# nums1 = int(input("Input a number of your choice, Again: "))
+
+
+#  if nums >= nums1:
+#      print("The first number you inputted was greater. ")
+#  else: print("The Second number you inputted was greater.")
+
+# if nums >= 0:
+#     print("Both of the Numbers are positive.")
+# else: print("One of the numbers are Negative.")
+
+
+# N4
+
+
+# name = (input("Enter name: "))
+# age = int(input("Enter age: "))
+
+# if age >= 21:
+#     print("User is older than 21")
+# else: print("User is not older than 21")
+
+
+
+# N5
+
+
+
+# num = int(input("Enter a number: "))
+# num1 = int(input("Enter a number: "))
+
+# print(type(num // num1))
+
+
+# N6
+
+
+
+# height_cm = float(input("Enter height in cm: "))
+
+# height_meter = height_cm / 100
+
+# is_tall = height_meter > 1.75
+
+# print(is_tall,)
+
+
+
+
+# N7
+
+
+
+# number = int(input("Input a number: "))
+
+# if number > 0:
+#     print("Number is positive")
+# elif number < 0:
+#     print("Number is negative")
+# else:
+#     print("The number is zero")
+
+
+
+
+# N8
+
+
+
+# user_input = input("Input a number: ")
+
+
+# print("Inputted data type:", type(user_input))
+
+
+# number = int(user_input)
+# print("Type after conversion to int:", type(number))
+
+
+# if number > 100:
+#     print("Number is greater than 100")
+# elif number < 100:
+#     print("Number is less than 100")
+# else:
+#     print("Number is 100")
+
+
+
+
+
+
+# N9
+
+
+
+# num1 = float(input("Input the first number: "))
+# num2 = float(input("Input the second number: "))
+# num3 = float(input("Input the third number: "))
+
+
+# average = (num1 + num2 + num3) / 3
+
+
+# greater_50 = average > 50
+
+
+# print("Average is:", average)
+# print("Is average greater than 50?", greater_50)
+
+
+# print("Average's type:", type(average))
+# print("Results's type:", type(greater_50))
+
+
+
+
+
+
+# N10?
+
+
+
+
+
+
+
