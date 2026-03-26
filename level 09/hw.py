510a01d90840e3b4c3c1e126b031cc4b56596410
alexmeladze <nesiakobadze@gmail.com>
2026-03-26 22:53:27 +0400

    second commit

---
+++ level 09/hw.py
@@ -0,0 +1,61 @@
+# listt = ["onion, apple, watermelon, banana, grapes"]
+# print(listt[0])
+# print(listt[1])
+# print(listt[2])
+# print(listt[3])
+# print(listt[4])
+
+
+
+# nums = ["12, 6, 14, 64, 28, 19, 60, 91, 77, 55"]
+
+# nums[0:3] = "100, 200, 300, 400, 500"
+# print(nums)
+
+
+
+# numbers = [5, 8, 2, 10, 3, 7, 1, 6, 4, 9]
+
+# total = 0
+# for num in numbers:
+#     total += num
+
+# print("Sum:", total)
+
+
+
+# numbers = []
+
+# for i in range(5):
+#     num = int(input("Enter a number: "))
+#     numbers.append(num)
+
+# total = 0
+# for num in numbers:
+#     total += num
+
+# print("Sum:", total)
+
+
+
+
+
+# numbers = [2, 4, 6, 8, 10]
+
+
+# for i in range(len(numbers)):
+#     numbers[i] += 1
+
+
+# print(numbers)
+
+
+
+# numbers = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28]
+
+# odd_count = 0
+# for num in numbers:
+#     if num % 2 != 0:
+#         odd_count += 1
+
+# print("Number of odd numbers:", odd_count)
